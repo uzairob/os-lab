d95c7401f7fb94dc9146948e4aba4d6753cd7e77
uzairob <160733216+uzairob@users.noreply.github.com>
2024-07-18 13:47:35 +0530

    Update

--- Page-Replacement.c
+++ Page-Replacement.c
@@ -168,3 +168,194 @@ int main()
 
     return 0;
 }
+
+
+//proper format ouptut
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+void printFrames(int F[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (F[i] == -1)
+            printf(" - ");
+        else
+            printf(" %d ", F[i]);
+    }
+}
+
+void fifo(int F[], int n, int P[], int m) {
+    int index = 0;
+    int p_fault = 0;
+
+    printf("The Page Replacement Process using FIFO is:\n");
+
+    for (int i = 0; i < m; i++) {
+        bool found = false;
+        for (int j = 0; j < n; j++) {
+            if (F[j] == P[i]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            F[index] = P[i];
+            index = (index + 1) % n;
+            p_fault++;
+            printf("%d ", P[i]);
+            printFrames(F, n);
+            printf(" PF No. %d\n", p_fault);
+        } else {
+            printf("%d ", P[i]);
+            printFrames(F, n);
+            printf("\n");
+        }
+    }
+
+    printf("The number of Page Faults using FIFO are %d\n", p_fault);
+}
+
+void lru(int F[], int n, int P[], int m) {
+    int LU[n];
+    int p_fault = 0;
+    int count = 0;
+
+    printf("The Page Replacement Process using LRU is:\n");
+
+    for (int i = 0; i < n; i++) {
+        F[i] = -1;
+        LU[i] = -1;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int j;
+        bool found = false;
+
+        for (j = 0; j < n; j++) {
+            if (F[j] == P[i]) {
+                found = true;
+                LU[j] = count++;
+                break;
+            }
+        }
+
+        if (!found) {
+            int lru_index = 0;
+            for (j = 1; j < n; j++) {
+                if (LU[j] < LU[lru_index]) {
+                    lru_index = j;
+                }
+            }
+            F[lru_index] = P[i];
+            LU[lru_index] = count++;
+            p_fault++;
+            printf("%d ", P[i]);
+            printFrames(F, n);
+            printf(" PF No. %d\n", p_fault);
+        } else {
+            printf("%d ", P[i]);
+            printFrames(F, n);
+            printf("\n");
+        }
+    }
+
+    printf("The number of Page Faults using LRU are %d\n", p_fault);
+}
+
+void optimal(int F[], int n, int P[], int m) {
+    int p_fault = 0;
+
+    printf("The Page Replacement Process using Optimal is:\n");
+
+    for (int i = 0; i < n; i++) {
+        F[i] = -1;
+    }
+
+    for (int i = 0; i < m; i++) {
+        bool found = false;
+        for (int j = 0; j < n; j++) {
+            if (F[j] == P[i]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            int replace_i = -1;
+            int far = i + 1;
+
+            for (int j = 0; j < n; j++) {
+                int k;
+                for (k = i + 1; k < m; k++) {
+                    if (F[j] == P[k]) {
+                        if (k > far) {
+                            far = k;
+                            replace_i = j;
+                        }
+                        break;
+                    }
+                }
+
+                if (k == m) {
+                    replace_i = j;
+                    break;
+                }
+            }
+
+            if (replace_i == -1) {
+                replace_i = 0;
+            }
+
+            F[replace_i] = P[i];
+            p_fault++;
+            printf("%d ", P[i]);
+            printFrames(F, n);
+            printf(" PF No. %d\n", p_fault);
+        } else {
+            printf("%d ", P[i]);
+            printFrames(F, n);
+            printf("\n");
+        }
+    }
+
+    printf("The number of Page Faults using Optimal are %d\n", p_fault);
+}
+
+int main() {
+    int n;
+    int m;
+
+    printf("Enter the number of Frames: ");
+    scanf("%d", &n);
+
+    printf("Enter the length of reference string: ");
+    scanf("%d", &m);
+
+    int P[m];
+    printf("Enter the reference string: ");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &P[i]);
+    }
+
+    int F[n];
+    for (int i = 0; i < n; i++) {
+        F[i] = -1;
+    }
+
+    fifo(F, n, P, m);
+
+    for (int i = 0; i < n; i++) {
+        F[i] = -1;
+    }
+
+    lru(F, n, P, m);
+
+    for (int i = 0; i < n; i++) {
+        F[i] = -1;
+    }
+
+    optimal(F, n, P, m);
+
+    return 0;
+}
